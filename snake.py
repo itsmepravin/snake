fa899dd847d2303598a1feeb197437f7544782b4
itsmepravin <singh.pravin19@yahoo.com>
2024-01-27 17:08:17 +0545

    snake head rect on screen

--- snake.py
+++ snake.py
@@ -9,6 +9,11 @@ TILE_SIZE = 25
 WINDOW_WIDTH = TILE_SIZE * COLS
 WINDOW_HEIGHT = TILE_SIZE * ROWS
 
+class Tile:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
 window = tkinter.Tk()
 window.title("Snake Game")
 window.resizable(False, False)
@@ -27,5 +32,16 @@ window_x = int((screen_width / 2) - (window_width / 2))
 window_y = int((screen_height / 2) - (window_height / 2))
 
 window.geometry(f"{window_width}x{window_height}+{window_x}+{window_y}")
+
+#Initialize Game
+snake = Tile(5*TILE_SIZE, 5*TILE_SIZE) #Snake's Head as Single Tile
+
+def draw():
+    global snake
+
+    #Draw Snake
+    canvas.create_rectangle(snake.x, snake.y, snake.x + TILE_SIZE, snake.y + TILE_SIZE, fill = "lime green")
+
+    window.after(100, draw) # Call draw function after every 100 milliseconds
 
 window.mainloop()
