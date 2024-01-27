4e1eebea96db21882b98786f9a20a5447202019c
itsmepravin <singh.pravin19@yahoo.com>
2024-01-27 16:57:01 +0545

    windows opens in center of screen

--- snake.py
+++ snake.py
@@ -5,11 +5,27 @@ ROWS = 25
 COLS = 25
 TILE_SIZE = 25
 
+#Game Window
 WINDOW_WIDTH = TILE_SIZE * COLS
 WINDOW_HEIGHT = TILE_SIZE * ROWS
 
 window = tkinter.Tk()
 window.title("Snake Game")
 window.resizable(False, False)
+
+canvas = tkinter.Canvas(window, bg = "black", width=WINDOW_WIDTH, height=WINDOW_HEIGHT, borderwidth=0, highlightthickness=0)
+canvas.pack()
+window.update()
+
+#Center the Window
+window_width = window.winfo_width()
+window_height = window.winfo_height()
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+
+window_x = int((screen_width / 2) - (window_width / 2))
+window_y = int((screen_height / 2) - (window_height / 2))
+
+window.geometry(f"{window_width}x{window_height}+{window_x}+{window_y}")
 
 window.mainloop()
